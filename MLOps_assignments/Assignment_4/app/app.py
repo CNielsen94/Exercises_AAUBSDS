7673ece322b1d8f598a54e3739c6abcab31d3f9e
CNielsen94 <112628536+CNielsen94@users.noreply.github.com>
2023-04-29 19:55:38 +0200

    Update app.py
    updated app.py with new interface

--- MLOps_assignments/Assignment_4/app/app.py
+++ MLOps_assignments/Assignment_4/app/app.py
@@ -7,21 +7,92 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import mlflow
 from PIL import Image
+import pickle
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
 import os
+
+# Set the page title and icon
+st.set_page_config(page_title="Employee Attrition Prediction App", page_icon=":guardsman:")
+
+# Add a big title to the top of the page
+st.title("Employee Attrition Prediction App")
+
+defaults = {'Age': 50,
+            'BusinessTravel': 'Travel_Rarely', 
+            'Department': 'Research & Development', 
+            'Education': 3, 
+            'EducationField': 'Life Sciences', 
+            'Gender': 'Male',
+            'JobLevel': 2,
+            'JobRole': 'Sales Representative',
+            'MaritalStatus': 'Married',
+            'NumCompaniesWorked': 1,
+            'PercentSalaryHike': 15,
+            'StockOptionLevel': 0,
+            'TrainingTimesLastYear': 3,
+            'YearsSinceLastPromotion': 0,
+            'YearsWithCurrManager': 0,
+            'JobInvolvement': 2,
+            'PerformanceRating': 3,
+            'EnvironmentSatisfaction': 3,
+            'JobSatisfaction': 3,
+            'WorkLifeBalance': 3
+           }
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # Database connection
-#print("Connecting to database...")
-#url = 'https://github.com/CNielsen94/Exercises_AAUBSDS/blob/main/MLOps_assignments/Assignment_three/Database/HR_DB.db?raw=true'
-#filename = 'HR_DB.db'
-#urllib.request.urlretrieve(url, filename)
-# Set the path to the local database file
-database_path = os.path.join(os.path.dirname(__file__), "HR_DB.db")
+print("Connecting to database...")
+url = 'https://github.com/CNielsen94/Exercises_AAUBSDS/blob/main/MLOps_assignments/Assignment_three/Database/HR_DB.db?raw=true'
+filename = 'HR_DB.db'
+urllib.request.urlretrieve(url, filename)
 
-# Connect to the SQLite database
-conn = sqlite3.connect(database_path)
+# Load the model from the pkl file
+with open(os.path.join(BASE_DIR, 'model.pkl'), 'rb') as f:
+    model = pickle.load(f)
+
+# Define the columns to collect from the user
+columns = ['Age', 'BusinessTravel', 'Department', 'Education', 'EducationField', 'Gender',
+           'JobLevel', 'JobRole', 'MaritalStatus', 'NumCompaniesWorked',
+           'PercentSalaryHike', 'StockOptionLevel', 'TrainingTimesLastYear',
+           'YearsSinceLastPromotion', 'YearsWithCurrManager', 'JobInvolvement',
+           'PerformanceRating', 'EnvironmentSatisfaction', 'JobSatisfaction',
+           'WorkLifeBalance']
+
+# Create a dictionary to store the user input
+user_input = {}
+
+# Collect the user input using Streamlit widgets
+for col in columns:
+    user_input[col] = st.text_input(col, defaults[col])
+
+# Convert the user input into a DataFrame
+input_df = pd.DataFrame(user_input, index=[0])
+input_df = input_df.fillna(defaults)
+
+#Create list of object data types in df
+others = input_df.select_dtypes('object').columns
+
+# Perform label encoding
+label_encoder = LabelEncoder()
+for col in others:
+    input_df[col] = label_encoder.fit_transform(input_df[col])
+
+# Perform standard scaling
+scaler = StandardScaler()
+input_df = scaler.fit_transform(input_df)
+
+# Make predictions using the model
+prediction = model.predict(input_df)
+
+# Display the prediction to the user
+if prediction == 0:
+    st.success('You are not likely to churn.')
+else:
+    st.success('You are likely to churn.')
+conn = sqlite3.connect('HR_DB.db')
 print("Connection established.")
-
-
-
 
 # Setting up dataframe for EDA
 print("Loading dataframes...")
@@ -58,37 +129,6 @@ run_id = "9e99a255ada3438badd5353d66b28e27"
 # Get the metrics of your model 
 metrics = mlflow.get_run(run_id).data.metrics
 metrics_df = pd.DataFrame(metrics.items(), columns=["Metric", "Value"])
-def app():
-    st.set_page_config(page_title="Employee Attrition Analysis", page_icon=":guardsman:", layout="wide")
-
-    st.title("Employee Attrition Analysis")
-
-    st.sidebar.header("Select an analysis")
-
-    analysis_type = st.sidebar.selectbox("", ["Exploratory Data Analysis (EDA)", "Predictive Modeling"])
-
-    if analysis_type == "Exploratory Data Analysis (EDA)":
-        st.subheader("Exploratory Data Analysis (EDA)")
-        st.title("Machine Learning Model Metrics")
-        st.write(metrics_df)
-        url = 'https://raw.githubusercontent.com/CNielsen94/Exercises_AAUBSDS/main/MLOps_assignments/Assignment_three/Database/mlruns/1/9e99a255ada3438badd5353d66b28e27/artifacts/feature_importances.png'
-        filename = 'feature_importances.png'
-        urllib.request.urlretrieve(url, filename)
-        img = Image.open("feature_importances.png")
-        st.image(img, caption="Feature Importances Plot")
-        # Age filter for talented employees
-        print("Filtering by age...")
-        df_EDA_u30 = df_EDA[df_EDA['Age'] < 30]
-
-        # Filter for employees with higher manager survey score than employee survey score
-        print("Filtering by survey score...")
-        talented_employees = df_EDA_u30[(df_EDA_u30['total_mn_y'] - df_EDA_u30['total_em']) > 1]
-
-        # Visualize attrition distribution
-        print("Visualizing data...")
-        fig, ax = plt.subplots()
-        sns.countplot(data=df_general, x='Attrition', hue='Department')
-        st.pyplot(fig)
 
 if __name__ == '__main__':
     app()
